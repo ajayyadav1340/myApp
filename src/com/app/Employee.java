14146d826510610d405544b24853aa9e0385707d
ADMIN <ADMIN@192.168.10.108>
2019-05-09 13:57:39 +0530

    Task 8671 employee setup done

---
+++ src/com/app/Employee.java
@@ -0,0 +1,19 @@
+package com.app;
+
+public class Employee {
+
+	private int empId;
+
+	public Employee() {
+		super();
+	}
+
+	public int getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+	
+}
